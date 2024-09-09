0b5ba53d74d5db171a0ad4a4711e7a0f80004638
Prnzdiamond <oseahumenagboifoh@gmail.com>
2024-09-09 05:25:27 -0700

    eybhj

--- src/components/JobListing.jsx
+++ src/components/JobListing.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { Link } from "react-router-dom";
 
 const JobListing = ({ job }) => {
   const [readMore, setReadMore] = useState(false);
@@ -11,37 +12,37 @@ const JobListing = ({ job }) => {
 
   return (
     <>
-      <div className="bg-white rounded-xl shadow-md relative" key={job.id}>
+      <div className="relative bg-white shadow-md rounded-xl" key={job.id}>
         {console.log(job.id)}
         <div className="p-4">
           <div className="mb-6">
-            <div className="text-gray-600 my-2">{job.type}</div>
+            <div className="my-2 text-gray-600">{job.type}</div>
             <h3 className="text-xl font-bold">{job.title}</h3>
           </div>
 
           <div className="mb-3">{description}</div>
           <button
             onClick={() => setReadMore((setReadMore) => !setReadMore)}
-            className="text-blue-600   hover:underline  inline-block"
+            className="inline-block text-blue-600 hover:underline"
           >
             {readMore ? "Less" : "More"}
           </button>
 
-          <h3 className="text-indigo-500 mb-2">{job.salary} / Year</h3>
+          <h3 className="mb-2 text-indigo-500">{job.salary} / Year</h3>
 
-          <div className="border border-gray-100 mb-5"></div>
+          <div className="mb-5 border border-gray-100"></div>
 
-          <div className="flex flex-col lg:flex-row justify-between mb-4">
-            <div className="text-orange-700 mb-3">
-              <i className="fa-solid fa-location-dot text-lg"></i>
+          <div className="flex flex-col justify-between mb-4 lg:flex-row">
+            <div className="mb-3 text-orange-700">
+              <i className="text-lg fa-solid fa-location-dot"></i>
               {job.location}
             </div>
-            <a
-              href={`/jobsPage/${job.id}`}
+            <Link
+              to={`/jobsPage/${job.id}`}
               className="h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm"
             >
               Read More
-            </a>
+            </Link>
           </div>
         </div>
       </div>
